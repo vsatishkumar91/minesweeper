070c404e0abe2f790bb6df909c030e4e6a0580d8
satish kumar <vsatishkumar91@users.noreply.github.com>
2021-08-23 13:38:14 +0530

    fillCluesCount added

--- app.js
+++ app.js
@@ -25,6 +25,30 @@ document.addEventListener('DOMContentLoaded', () => {
         squares.push(square);
       }
     }
+    fillCluesCount();
+  }
+
+  function fillCluesCount() {
+    for (let i = 0; i < width * width; i++) {
+      let totalBomb = 0;
+      const isLeftEdge = i % width == 0;
+      const isRightEdge = i % width == width - 1;
+      const isTopEdge = i < width;
+      const isBottomEdge = Math.floor(i/width) == width - 1;
+
+      if (!isTopEdge && !isLeftEdge && squares[i - width - 1].classList.contains('bomb')) totalBomb++;
+      if (!isTopEdge && !isRightEdge && squares[i - width + 1].classList.contains('bomb')) totalBomb++;
+      if (!isLeftEdge && squares[i - 1].classList.contains('bomb')) totalBomb++;
+      if (!isRightEdge && squares[i + 1].classList.contains('bomb')) totalBomb++;
+
+      if (!isBottomEdge && !isLeftEdge && squares[i + width - 1].classList.contains('bomb')) totalBomb++;
+      if (!isBottomEdge && !isRightEdge && squares[i + width + 1].classList.contains('bomb')) totalBomb++;
+
+      if (!isTopEdge && squares[i - width].classList.contains('bomb')) totalBomb++;
+      if (!isBottomEdge && squares[i + width].classList.contains('bomb')) totalBomb++;
+
+      squares[i].innerHTML = totalBomb;
+    }
   }
 
   function revealClue(e) {
