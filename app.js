7fefc41cee34d15146cfae19c49fbd87629efa89
satish kumar <vsatishkumar91@users.noreply.github.com>
2021-08-23 13:51:44 +0530

    click on square logic added.

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  const grid = document.querySelector('.grid');
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
 
   const width = 10;
   const bombsCount = 2 * width;
-
-
-
 
   let squares = [];
   let isGameOver = false;
@@ -14,15 +10,19 @@ document.addEventListener('DOMContentLoaded', () => {
   function createBoard() {
     const bombsList = Array(bombsCount).fill("bomb");
     const emptyList = Array(width * width - bombsCount).fill("empty");
-    const totalList = bombsList.concat(emptyList).sort(() => Math.random() - 0.5);
+    const totalList = bombsList
+      .concat(emptyList)
+      .sort(() => Math.random() - 0.5);
     for (let i = 0; i < 10; i++) {
       for (let j = 0; j < 10; j++) {
-        const square = document.createElement('div');
-        square.setAttribute('id', i * width + j);
+        const square = document.createElement("div");
+        square.setAttribute("id", i * width + j);
         square.classList.add(totalList[i * width + j]);
-        square.addEventListener('click', revealClue);
         grid.appendChild(square);
         squares.push(square);
+        square.addEventListener("click", function (e) {
+          revealClue(square);
+        });
       }
     }
     fillCluesCount();
@@ -30,36 +30,71 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function fillCluesCount() {
     for (let i = 0; i < width * width; i++) {
-      let totalBomb = 0;
-      const isLeftEdge = i % width == 0;
-      const isRightEdge = i % width == width - 1;
-      const isTopEdge = i < width;
-      const isBottomEdge = Math.floor(i/width) == width - 1;
+      if (!squares[i].classList.contains("bomb")) {
+        let totalBomb = 0;
+        const isLeftEdge = i % width == 0;
+        const isRightEdge = i % width == width - 1;
+        const isTopEdge = i < width;
+        const isBottomEdge = Math.floor(i / width) == width - 1;
+        if (
+          !isTopEdge &&
+          !isLeftEdge &&
+          squares[i - width - 1].classList.contains("bomb")
+        )
+          totalBomb++;
+        if (
+          !isTopEdge &&
+          !isRightEdge &&
+          squares[i - width + 1].classList.contains("bomb")
+        )
+          totalBomb++;
+        if (!isLeftEdge && squares[i - 1].classList.contains("bomb"))
+          totalBomb++;
+        if (!isRightEdge && squares[i + 1].classList.contains("bomb"))
+          totalBomb++;
 
-      if (!isTopEdge && !isLeftEdge && squares[i - width - 1].classList.contains('bomb')) totalBomb++;
-      if (!isTopEdge && !isRightEdge && squares[i - width + 1].classList.contains('bomb')) totalBomb++;
-      if (!isLeftEdge && squares[i - 1].classList.contains('bomb')) totalBomb++;
-      if (!isRightEdge && squares[i + 1].classList.contains('bomb')) totalBomb++;
+        if (
+          !isBottomEdge &&
+          !isLeftEdge &&
+          squares[i + width - 1].classList.contains("bomb")
+        )
+          totalBomb++;
+        if (
+          !isBottomEdge &&
+          !isRightEdge &&
+          squares[i + width + 1].classList.contains("bomb")
+        )
+          totalBomb++;
 
-      if (!isBottomEdge && !isLeftEdge && squares[i + width - 1].classList.contains('bomb')) totalBomb++;
-      if (!isBottomEdge && !isRightEdge && squares[i + width + 1].classList.contains('bomb')) totalBomb++;
+        if (!isTopEdge && squares[i - width].classList.contains("bomb"))
+          totalBomb++;
+        if (!isBottomEdge && squares[i + width].classList.contains("bomb"))
+          totalBomb++;
 
-      if (!isTopEdge && squares[i - width].classList.contains('bomb')) totalBomb++;
-      if (!isBottomEdge && squares[i + width].classList.contains('bomb')) totalBomb++;
-
-      squares[i].innerHTML = totalBomb;
+        squares[i].innerHTML = totalBomb;
+        squares[i].setAttribute("totalBombs", totalBomb);
+      }
     }
   }
 
-  function revealClue(e) {
-    console.log(e.id);
-  }
+  function revealClue(square) {
+    if (isGameOver) return;
+    if (square.classList.contains("visited")) return;
+    if (square.classList.contains("bomb")) {
+      alert("Game Over");
+      isGameOver = true;
+    } else {
+      const bombsCount = parseInt(square.getAttribute(totalBombs));
 
+      if (bombsCount) {
+        square.classList.add("visited");
+      }
+    }
+  }
 
   function init() {
     createBoard();
   }
 
   init();
-
 });
